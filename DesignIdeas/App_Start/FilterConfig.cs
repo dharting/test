67fbc785776c98b2535f34105806879cf722d32a
dharting <dharting@bhsolutions.com>
2013-06-18 11:52:14 -0400

    file changed

---
+++ DesignIdeas/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DesignIdeas
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
